6ae808db4c9b0ca376a348efd9c2c906c1de7643
Shihab <67054762+internet-help-zone-shihab@users.noreply.github.com>
2020-07-13 12:15:48 +0600

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -8,15 +8,15 @@ yellow='\e[1;33m'
 echo ""
 echo""
 banner() {
+	echo -e $'\e[1;33m\e[0m\e[1;31m            "#################################" \e[0m'
+	echo -e $'\e[1;33m\e[0m\e[1;32m            "#      ~Advance Phishing~🚩     #" \e[0m'
+	echo -e $'\e[1;33m\e[0m\e[1;33m            "#################################" \e[0m'
+	echo -e $'\e[1;33m\e[0m\e[1;34m            "#     ~MOHAMMAD-SHIHAB-UDDIN~   #" \e[0m'
 	echo -e $'\e[1;33m\e[0m\e[1;37m            "#################################" \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#      ~Advance Phishing~🚩     #" \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#################################" \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#     ~MOHAMMAD-SHIHAB-UDDIN~   #" \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#################################" \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#     ~INTERNET-HELP-ZONE~      #" \e[0m'          
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#################################" \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#   ~BANGLADESH-NOOB-HACKERS~   #" \e[0m'
-	echo -e $'\e[1;33m\e[0m\e[1;37m            "#################################" \e[0m'
+	echo -e $'\e[1;33m\e[0m\e[1;32m            "#     ~INTERNET-HELP-ZONE~      #" \e[0m'          
+	echo -e $'\e[1;33m\e[0m\e[1;33m            "#################################" \e[0m'
+	echo -e $'\e[1;33m\e[0m\e[1;34m            "#   ~BANGLADESH-NOOB-HACKERS~   #" \e[0m'
+	echo -e $'\e[1;33m\e[0m\e[1;31m            "#################################" \e[0m'
    echo ""
    echo -e $'\e[1;33m\e[0m\e[1;33m            [ \e[0m\e[1;32m Cracked by SHIHAB \e[0m \e[1;32m\e[0m\e[1;33m ] \e[0m'
    echo ""
